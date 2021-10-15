3538284dc8dc8675976bdd5ee58e92e397f00db7
JD <jdmejia712@gmail.com>
2021-10-15 10:08:47 -0400

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+
+
+function findMatching(collection, name){
+ 
+    return collection.filter(driver => {
+        return driver.toLowerCase()=== name.toLowerCase()})
+    }
+    
+    // return !!thisDriver ? thisDriver : undefined
+
+
+function fuzzyMatch(collection, partName){
+    let partial = collection.filter(driver => driver.slice(0, partName.length) === partName)
+    return !!partial ? partial : undefined
+}
+
+// function matchName(collection){
+//     let nameCheck = collection.filter(driver => driver.name === collection.driver.name)
+//     return nameCheck
+// }
+function matchName(collection, name){
+ 
+    return collection.filter(driver => {
+        return driver.name.toLowerCase()=== name.toLowerCase()})
+    }
+   
